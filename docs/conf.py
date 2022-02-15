9eef0986b1afe0d2a544a006cd68a55ae3a0cf15
Hailey Nichols <nichols.hailey@gmail.com>
2022-02-15 12:58:22 -0600

    fix error

--- docs/conf.py
+++ docs/conf.py
@@ -38,19 +38,19 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = [
-#     'sphinx_rtd_theme',
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.viewcode',
-#     'sphinx.ext.githubpages',
-# ]
-
 extensions = [
     'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_tabs.tabs',
 ]
+
+# extensions = [
+#     'sphinx_rtd_theme',
+#     'sphinx.ext.githubpages',
+#     'sphinx.ext.autosectionlabel',
+#     'sphinx_tabs.tabs',
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
