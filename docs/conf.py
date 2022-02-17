2f6c71d1d7c2be2fb8218aad70a9a1798d9f2376
locuslock <99498377+locuslock@users.noreply.github.com>
2022-02-17 14:06:10 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    #'sphinx.ext.autosectionlabel'
+    'sphinx.ext.autosectionlabel'
     #'sphinx.ext.intersphinx',
 ]
 
